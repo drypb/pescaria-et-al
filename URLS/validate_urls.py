e903159c3edfff61692adf01e45d552bd5a93547
pedro f <phfr24@inf.ufpr.br>
2025-03-12 15:29:51 -0300

    add URLS/validate_urls.py

---
+++ URLS/validate_urls.py
@@ -0,0 +1,18 @@
+from urllib.parse import urlparse
+import sys
+
+def valid(url):
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc])
+    except AttributeError:
+        return False
+
+def main():
+    with open(sys.argv[1], 'r') as file:
+        for url in file:
+            if not valid(url):
+                print(url + ' is invalid !')
+
+if __name__ == "__main__":
+    main()
